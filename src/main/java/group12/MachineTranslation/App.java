d0d1bd18836762fb62154cf583329794f48efa33
carl999 <snxsdny@qq.com>
2017-07-17 15:00:10 +0800

    mt

---
+++ src/main/java/group12/MachineTranslation/App.java
@@ -0,0 +1,13 @@
+package group12.MachineTranslation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
