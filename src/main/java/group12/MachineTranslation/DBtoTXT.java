fd47890a9eaa958b2c59b52d46ee0f2a997deefe
carl999 <snxsdny@qq.com>
2017-07-26 09:50:55 +0800

    aaaa

--- src/main/java/group12/MachineTranslation/DBtoTXT.java
+++ src/main/java/group12/MachineTranslation/DBtoTXT.java
@@ -1,50 +1,48 @@
 package group12.MachineTranslation;
-import java.io.BufferedWriter;
+
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.RandomAccessFile;
-
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
 import com.mongodb.MongoClient;
-import com.mongodb.client.MongoDatabase;
+
 
 public class DBtoTXT {
 
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
-        MongoClient mongoClient = new MongoClient( "localhost" , 27017 );
-        DB db = mongoClient.getDB("mydb");
-        DBCollection dbColl = db.getCollection("Comment");
-        DBCursor cursor = dbColl.find();        
-        
-        int i = 0;
-        while(cursor.hasNext()){
-        	if(i<40000){
-    		try {
-    			writeTxtFile(cursor.next().toMap().get("summary").toString()+"\r\n", new File("C:/Users/Lenovo/Desktop/s2s/data/giga-fren.release2.fixed.en"));
-    			writeTxtFile(cursor.next().toMap().get("score").toString().charAt(0)+"\r\n", new File("C:/Users/Lenovo/Desktop/s2s/data/giga-fren.release2.fixed.fr"));
-    			System.out.println(i);
-    			i++;
-    		} catch (Exception e) {
-    			// TODO 自动生成的 catch 块
-    			e.printStackTrace();
-    		}
-        	}else{
-    			try {
-					writeTxtFile(cursor.next().toMap().get("summary").toString()+"\r\n", new File("C:/Users/Lenovo/Desktop/s2s/data/newstest2013.en"));
-					writeTxtFile(cursor.next().toMap().get("score").toString().charAt(0)+"\r\n", new File("C:/Users/Lenovo/Desktop/s2s/data/newstest2013.fr"));
-					System.out.println(i+"!");
-					i++;
-				} catch (Exception e) {
-					// TODO 自动生成的 catch 块
-					e.printStackTrace();
-				}
-    			
-        	}
-        }
+//        MongoClient mongoClient = new MongoClient( "localhost" , 27017 );
+//        DB db = mongoClient.getDB("mydb");
+//        DBCollection dbColl = db.getCollection("Comment");
+//        DBCursor cursor = dbColl.find();        
+//        
+//        int i = 0;
+//        while(cursor.hasNext()){
+//        	if(i<40000){
+//    		try {
+//    			writeTxtFile(cursor.next().toMap().get("summary").toString()+"\r\n", new File("C:/Users/Lenovo/Desktop/s2s/data/giga-fren.release2.fixed.en"));
+//    			writeTxtFile(cursor.next().toMap().get("score").toString().charAt(0)+"\r\n", new File("C:/Users/Lenovo/Desktop/s2s/data/giga-fren.release2.fixed.fr"));
+//    			System.out.println(i);
+//    			i++;
+//    		} catch (Exception e) {
+//    			// TODO 自动生成的 catch 块
+//    			e.printStackTrace();
+//    		}
+//        	}else{
+//    			try {
+//					writeTxtFile(cursor.next().toMap().get("summary").toString()+"\r\n", new File("C:/Users/Lenovo/Desktop/s2s/data/newstest2013.en"));
+//					writeTxtFile(cursor.next().toMap().get("score").toString().charAt(0)+"\r\n", new File("C:/Users/Lenovo/Desktop/s2s/data/newstest2013.fr"));
+//					System.out.println(i+"!");
+//					i++;
+//				} catch (Exception e) {
+//					// TODO 自动生成的 catch 块
+//					e.printStackTrace();
+//				}
+//    			
+//        	}
+//        }
 
 	}
 	
